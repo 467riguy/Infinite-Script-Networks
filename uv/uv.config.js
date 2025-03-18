c8d4a2790003bd97954cabe25d3e3f840b87b0e4
Devman <116588566+xdevman9@users.noreply.github.com>
2025-03-18 09:29:39 -0700

    Initial commit

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
 	prefix: "/service/",
+	bare: "https://bare-2wn.pages.dev/", // Bare server Url
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/uv.handler.js",
